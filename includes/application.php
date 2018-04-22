06eb17395e63f9ad2e407f45066d3078a893757c
Bolrod <34643041+Bolrod@users.noreply.github.com>
2018-04-22 15:55:13 +0200

    application

---
+++ includes/application.php
@@ -0,0 +1,52 @@
+<?php
+require_once "includes/functions.php";
+?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!doctype html>
+<html>
+
+<?php require_once "includes/head.php"; ?>
+
+<body>
+    <div class="container">
+        <?php require_once "includes/header.php"; ?>
+
+<h3> <span class="glyphicon glyphicon-star"> </span> Qui ? </h3>
+<ul>
+<li>Toute personne étant intéressée par l’effet du son sur le cerveau.</li>
+<li>Le public souhaitant se relaxer, se concentrer.</li>
+<li>Tout public voulant se divertir.</li>
+</ul>
+
+<h3> <span class="glyphicon glyphicon-star"> </span> Quoi ? </h3>
+<ul>
+<li>Application dont le but est de relaxer, favoriser la concentration dans un contexte agréable </li>
+<li>Enjeux de santé publique</li>
+</ul>
+
+<h3> <span class="glyphicon glyphicon-star"> </span>Où ? </h3>
+<ul>
+<li>Dans un lieu calme</li>
+</ul>
+
+<h3> <span class="glyphicon glyphicon-star"> </span> Quand ? </h3>
+<ul>
+<li>A tout moment où l’utilisateur souhaite se détendre, se reposer ou dormir.</li>
+</ul>
+
+<h3> <span class="glyphicon glyphicon-star"> </span> Comment ? </h3>
+<ul> <li> Application sous Unity avec le téléphone / tablette </li>
+<li>Casque de VR </li>
+<li>Ecouteur / Casque de musique </li>
+</ul>
+
+
+        <?php require_once "includes/footer.php"; ?>
+    </div>
+
+    <?php require_once "includes/scripts.php"; ?>
+</body>
+
+</html>
+
