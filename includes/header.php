461626c995da03186b120f05fa9507e91fbbaeec
Bolrod <34643041+Bolrod@users.noreply.github.com>
2018-04-22 15:54:09 +0200

    header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <?php require_once "includes/functions.php"; ?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container">
@@ -8,16 +9,24 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-film"></span> E-mind machine</a>
+            <a class="navbar-brand" href="index.php"><p class="title"> E-mind machine</p></a>
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse-target">
-            <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <span class="glyphicon glyphicon-user"></span> Bienvenue<b class="caret"></b>
+            <ul class="nav navbar-nav">
+                    <li>
+                        <a href="application.php" >
+                            L'application
                         </a>
                     </li>
+                    <li>
+                        <a href="organisation.php">
+                        L'organisation
+                    </li>
+                    <li>
+                     <a href="livrables.php">
+                     Les livrables
+                     </li>
             </ul>
         </div>
-    </div><!-- /.container -->
+    </div>
 </nav>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once "includes/functions.php";
 ?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
 <!doctype html>
 <html>
@@ -11,7 +12,13 @@ require_once "includes/functions.php";
     <div class="container">
         <?php require_once "includes/header.php"; ?>
 
-        Bonjour !
+        Bonjour, bienvenu sur le site du projet transdisciplinaire e-mind machine. Ce projet consiste à ...
+
+<h3 class="w3-blue" id="ecole">Notre école : L'ENSC</h3>
+<img src="images/ensc.jpg" alt="Logo ENSC" title="ENSC" class="image small">
+
+				<p>L'ENSC est une école d'ingénieurs de Bordeaux-INP [...]</p>
+				<p><a href="https://www.ensc.fr/" target="_blank" >En savoir plus</a></p>
 
         <?php require_once "includes/footer.php"; ?>
     </div>
