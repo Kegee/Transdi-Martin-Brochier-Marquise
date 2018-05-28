8072d1ae74eba2e1484734896ce0910c5a54d491
Bolrod <34643041+Bolrod@users.noreply.github.com>
2018-05-28 19:04:58 +0200

    index2

---
+++ includes/organisation.php
@@ -0,0 +1,51 @@
+<?php
+require_once "functions.php";
+?>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+<!doctype html>
+<html>
+
+<?php require_once "headIncludes.php"; ?>
+
+<body>
+
+<?php require_once "headerIncludes.php"; ?>
+  <div class="container">
+  
+    <h1> Pierre-Henry Vulliard <h1>
+
+    <p>
+    
+    Chercheur au Laboratoire Bordelais de Recherche en Informatique (LaBRI), 
+    Pierre-Henry est à l'origine de cette application. 
+    
+    Parler de ses concerts
+    Parler également de son autre appli et mettre un lien vers son site
+    </p>
+
+    <h1> Anne </h1>
+
+    <p>
+    A compléter
+    </p>
+
+    <h1> L'équipe transdiciplinaire de l'ENSC </h1>
+
+    <p>
+    A compléter
+    </p>
+
+    <h1> L'enseirb </h1>
+
+    <p>
+    A compléter
+    </p>
+
+  
+  </div>
+    <?php require_once "footer.php"; ?>
+    <?php require_once "scripts.php"; ?>
+</body>
+
+</html>
